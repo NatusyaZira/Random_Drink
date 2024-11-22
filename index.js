22150297018b660d005905c8ddf4e427ebb1515d
Nataliia Kovalova <natusya.zira@outlook.com>
2024-11-22 21:49:31 +0200

    Retrieve data from public API

--- index.js
+++ index.js
@@ -4,8 +4,27 @@ import axios from "axios";
 
 const app = express();
 const port = 3000;
+const API_URL = "https://thecocktaildb.com/api/json/v1/1/random.php";
 
 app.use(bodyParser.urlencoded({ extended: true }));
+
+
+app.get("/", async (req, res) => {
+    try {
+        const response = await axios.get(API_URL);
+        const result = response.data.drinks[0];
+        
+        console.log(result);
+        res.render("index.ejs", {data: result});
+    } catch (error) {
+        console.error("Failed to make request:", error.message);
+        res.render("index.ejs", {
+            error: error.message,
+        });
+    }
+});
+
+
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
